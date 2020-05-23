633983c6992a0d121fef451f824f15f5cec8babd
CRSPN <doeraysme@gmail.com>
2020-05-23 17:34:10 -0600

    reaserched js methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,37 @@
-var answerBtnA = document.getElementById("answer-a")
-var answerBtnB = document.getElementById("answer-b")
-var answerBtnC = document.getElementById("answer-c")
-var answerBtnD = document.getElementById("answer-d")
+var answerBtnA = document.getElementById("answer-a");
+var answerBtnB = document.getElementById("answer-b");
+var answerBtnC = document.getElementById("answer-c");
+var answerBtnD = document.getElementById("answer-d");
 
 //loop the array(s)? of answer buttons and Qs?
 
 //math.random to shuffle the order of the Ans btns
-var getPlName = function() {
-    var name = "";
-    
-        while (name === "" || name === null) {
-            name = prompt("please enter your name.");
-        }
 
-    console.log("Your name is " + name);
-    return name;
-};
 
-var timerPromt = alert("This is a timed quiz, timer= +time if correct/-time if incorrect ");
-
-var stats = [
-    name = getPlName(),
-    time = PlTimeleft.text("time left:" + timeLeft + "sec."),
-]
+//var timerPromt = alert("This is a timed quiz, timer= +time if correct/-time if incorrect ");
 
 
 
-answerBtnA.innerHTML = questions[0].a[0].text
-answerBtnB.innerHTML = questions[0].a[1].text
-answerBtnC.innerHTML = questions[0].a[2].text
-answerBtnD.innerHTML = questions[0].a[3].text
+
+
  
 
 var questions = [
     { q: 'Wrapping content in a container div element will do what?', a: [
-        {text:"button 1",isCorrect:true},
-        {text:"button 2", isCorrect:false},
-        {text:"button 3", isCorrect:false},
-        {text:"button 4", isCorrect:false},
+        {text:"sync and bundle all that content together",isCorrect:true},
+        {text:"activate the self destruct sequence", isCorrect:false},
+        {text:"hide everything in the div element", isCorrect:false},
+        {text:"give all your info to the north pole", isCorrect:false},
         
     ]},
-    { q: 'Wrapping content in a container div element will do what?', a: [
-        {text:"button 1",isCorrect:true},
-        {text:"button 2", isCorrect:false},
-        {text:"button 3", isCorrect:false},
-        {text:"button 4", isCorrect:false},
+    { q: 'a new DOM element property called innerHTML that works a lot like the textContent property, but with one big difference', a: [
+        {text:"the textContent property only accepts text content values",isCorrect:true},
+        {text:"all elements are turned to spanish", isCorrect:false},
+        {text:"there is no diffirence", isCorrect:false},
+        {text:"the innerHTML will turn anyone who looks at it to stone", isCorrect:false},
         
     ]},
-    { q: 'Wrapping content in a container div element will do what?', a: [
+    { q: '', a: [
         {text:"button 1",isCorrect:false},
         {text:"button 2", isCorrect:false},
         {text:"button 3", isCorrect:true},
@@ -61,6 +45,19 @@ var questions = [
         {text:"button 4", isCorrect:false},
         
     ]},
-]
-// maybe if {Question (); [ ]}
+] 
+answerBtnA.innerHTML = questions[0].a[0].text
+answerBtnB.innerHTML = questions[0].a[1].text
+answerBtnC.innerHTML = questions[0].a[2].text
+answerBtnD.innerHTML = questions[0].a[3].text
 
+var quizStart = function(questions) {
+   if(a === true) {
+       //add time
+
+   }
+    
+
+}
+
+questions.addEventListener("click", quizStart);
